5ff76a7cfec59f129e191ae70c8a6b0ea5f501cb
JeremyRONK_cp <SND\JeremyRONK_cp>
2012-09-12 15:58:43 +0000

    Mise en place du programme de setup

--- BizTalkZombieManagement/BizTalkZombieManagement/ProjectInstaller.cs
+++ BizTalkZombieManagement/BizTalkZombieManagement/ProjectInstaller.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.Linq;
+using System.ServiceProcess;
 
 
 namespace BizTalkZombieManagement
@@ -14,6 +15,27 @@ namespace BizTalkZombieManagement
         public ProjectInstaller()
         {
             InitializeComponent();
+            
+            //Configuration service
+
+            this.ZombieManagementServiceInstaller.StartType = ServiceStartMode.Automatic;
+            this.ZombieManagementServiceInstaller.Description = "Service to handle BizTalk Zombie instance";
+            this.ZombieManagementServiceInstaller.ServicesDependedOn = new String[] { "ENTSSO" };
+            //Setting credential
+            this.serviceProcessInstaller1.Account = ServiceAccount.User;
+
+            this.AfterInstall += new InstallEventHandler(AfterInstallService);
+        }
+
+        private void AfterInstallService(object send, InstallEventArgs e)
+        {
+            //start the service
+            ServiceController sc = new ServiceController("ZombieManagementService");
+            sc.Start();
+        }
+        public override void Commit(IDictionary savedState)
+        {
+            
         }
     }
 }
