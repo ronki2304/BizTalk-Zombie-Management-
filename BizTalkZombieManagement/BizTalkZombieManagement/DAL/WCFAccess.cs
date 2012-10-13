03dc3dcf0a684b08a1485b08568a3c56d7ced576
JeremyRONK_cp <SND\JeremyRONK_cp>
2012-10-13 10:44:38 +0000

    New Transport Layer : WCF

---
+++ BizTalkZombieManagement/BizTalkZombieManagement/DAL/WCFAccess.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel.Channels;
+using System.IO;
+using System.Xml;
+using System.ServiceModel;
+
+namespace BizTalkZombieManagement.DAL
+{
+    public class WCFAccess
+    {
+        ChannelFactory<Microsoft.BizTalk.Adapter.Wcf.Runtime.ITwoWayAsyncVoid> factory;
+
+        public WCFAccess(String EndPointName="BTSEndPoint")
+        {
+            factory = new ChannelFactory<Microsoft.BizTalk.Adapter.Wcf.Runtime.ITwoWayAsyncVoid>(EndPointName);
+        }
+        public void sendMessage (String message)
+        {
+            
+            var client = factory.CreateChannel();
+
+            //create the new message to send over channel
+            System.ServiceModel.Channels.Message msg = System.ServiceModel.Channels.Message.CreateMessage(System.ServiceModel.Channels.MessageVersion.Default
+                , "BeginTwoWayMethod"
+                , new XmlTextReader(new StringReader(message)));
+
+            //only async method exist
+            IAsyncResult res = client.BeginTwoWayMethod(msg, null, null);
+
+
+            //Blocks current thread until until AsyncWaitHandle receives a 
+            res.AsyncWaitHandle.WaitOne();
+
+
+            //End the call once the wait handle signal is received from BizTalk
+
+            client.EndTwoWayMethod(res);
+            ((IClientChannel)client).Close();
+        }
+    }
+}
---
+++ BizTalkZombieManagement/BizTalkZombieManagement/Interface/ITwoWayAsyncVoid.cs
@@ -0,0 +1,28 @@
+﻿using System.ServiceModel;
+using System;
+using System.ServiceModel.Channels;
+namespace Microsoft.BizTalk.Adapter.Wcf.Runtime
+{
+    /// <summary>
+    /// BizTalk Hack, the original class that implement this interface is not complete and we cannot herits from the original interface,
+    /// So I recreate it for sending message without any tag
+    /// </summary>
+    [ServiceContract(Namespace = "http://www.microsoft.com/biztalk/2006/r2/wcf-adapter")]
+    public interface ITwoWayAsyncVoid
+    {
+
+        // Methods
+
+        [OperationContract(AsyncPattern = true, IsOneWay = false, Action = "*", ReplyAction = "*")]
+
+        IAsyncResult BeginTwoWayMethod(System.ServiceModel.Channels.Message message, AsyncCallback callback, object state);
+
+        [OperationContract(IsOneWay = false, Action = "BizTalkSubmit")]
+
+        void BizTalkSubmit(System.ServiceModel.Channels.Message message);
+
+        void EndTwoWayMethod(IAsyncResult result);
+
+    }
+
+}
