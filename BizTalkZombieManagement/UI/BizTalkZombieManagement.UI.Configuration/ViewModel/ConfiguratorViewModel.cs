773e983c73ff2b715cb019370a6d0150049910fe
JeremyRONK_cp <SND\JeremyRONK_cp>
2012-10-15 20:25:10 +0000

    start configuration tool remove some warning analysis

---
+++ BizTalkZombieManagement/UI/BizTalkZombieManagement.UI.Configuration/ViewModel/ConfiguratorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace BizTalkZombieManagement.UI.Configuration.ViewModel
+{
+    public class ConfiguratorViewModel
+    {
+        #region Commandes
+        public ICommand ClickCommand { get; set; }
+        #endregion
+
+    }
+}
